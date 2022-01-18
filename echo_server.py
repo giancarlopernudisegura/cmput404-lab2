fd4532690afb9d17787534e7a7ab21a34a250e45
Giancarlo Pernudi Segura <pernudig@gmail.com>
2022-01-17 17:24:38 -0700

    q3 + q4

--- echo_server.py
+++ echo_server.py
@@ -2,19 +2,20 @@ import socket
 
 
 def main():
-    try:
-        host = ''
-        port = 8080
-        sock = socket.socket(
+    with socket.socket(
                 socket.AF_INET,
-                socket.SOCK_STREAM)
-        sock.bind((host, port))
-        sock.listen(1)
-        conn, addr = sock.accept()
-        print(conn)
-        print(addr)
-    except socket.error as err:
-        print('connection error:', err)
+                socket.SOCK_STREAM) as sock:
+        try:
+            host = ''
+            port = 8080
+            payload = 4096
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            sock.bind((host, port))
+            sock.listen(1)
+            conn, addr = sock.accept()
+            print(conn.recv(payload))
+        except socket.error as err:
+            print('connection error:', err)
 
 if __name__ == "__main__":
     main()
